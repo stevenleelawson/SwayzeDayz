d055581fc7333fc62a66b756c4e608b439bcf976
Steven Lawson <unluckygamblermusic@gmail.com>
2016-05-17 16:08:00 -0600

    full api list and now appends to screen

--- data.js
+++ data.js
@@ -1,52 +1,72 @@
 $(document).ready(function (){
-  var whatFilm;
-  var p1;
-  var p2;
-  var p3;
+
   $(".swayzeMe").on("click", function(){
-    whatFilm = $('#1').val().toLowerCase();
-    // console.log(whatFilm);
-    getData().then(function(data){
-      compareMovie(data,whatFilm);
+    var inputMovie = $('#1').val().toLowerCase();
+    // console.log(inputMovie);
+    getData().then(function(movieResults){
+      compareMovie(movieResults,inputMovie);
 
     });
 
   });
   function getData (){
-     p1=$.get("https://api.themoviedb.org/3/discover/movie?with_cast=723&api_key=426006a23834affecd834cfdf4937e2c")
+       var p1 = $.get("https://api.themoviedb.org/3/discover/movie?with_cast=723&api_key=426006a23834affecd834cfdf4937e2c")
     //  .done(function(data) {
 
     // });
-     p2=$.get("https://api.themoviedb.org/3/discover/movie?with_cast=723&page=2&api_key=426006a23834affecd834cfdf4937e2c")
+     var p2 = $.get("https://api.themoviedb.org/3/discover/movie?with_cast=723&page=2&api_key=426006a23834affecd834cfdf4937e2c")
     //  .done(function(data) {
 
     //  });
 
-     p3=$.get("https://api.themoviedb.org/3/discover/movie?with_cast=723&page=3&api_key=426006a23834affecd834cfdf4937e2c")
+     var p3 = $.get("https://api.themoviedb.org/3/discover/movie?with_cast=723&page=3&api_key=426006a23834affecd834cfdf4937e2c")
     // //  .done(function(data) {
     // console.log(compareMovie(data,whatFilm));
     // // });
-    return Promise.all([p1, p2, p3]);
+      return Promise.all([p1, p2, p3]);
 
   }
 
-  function compareMovie (data,whatFilm){
-    console.log(whatFilm);
-    var results=data.results;
-    var title;
-    for(i=0; i < results.length; i++){
-      title=results[i].title.toLowerCase();
-      // console.log(whatFilm,title);
-      console.log(whatFilm);
-        if(whatFilm===title){
-          console.log("SwayzeDay");
-        //.append()
-        } else {
-        console.log("Not a SwayzeDay");
-        //.append()
+  function compareMovie (movieResults,inputMovie){
+    var SwayzeDay=false;
+    // var SwayzeDay=(title===inputMovie);
+    for(i=0; i<movieResults.length; i++){
+      // console.log(data[i].results);
+      for (var j = 0; j < movieResults[i].results.length; j++) {
+        // console.log(data[i].results[j]);
+        var title=movieResults[i].results[j].title.toLowerCase();
+        // console.log(data[i].results[j].title);
+        if(title===inputMovie){
+          SwayzeDay=true;
+        }
       }
     }
-    console.log(whatFilm,data);
-  }
+    $('.ahpend').empty()
+  if(SwayzeDay===false){
 
-})
+    $('.ahpend').append("Not A SwayzeDay!")
+  } else {
+    
+
+    $('.ahpend').append("SwayzeDay")
+  }
+}
+    // console.log(whatFilm);
+    // results=dataresults;
+    // var title;
+    // for(i=0; i < results.length; i++){
+    //   title=results[i].title.toLowerCase();
+    //   // console.log(whatFilm,title);
+    //   console.log(whatFilm);
+    //     if(whatFilm===title){
+    //       console.log("SwayzeDay");
+    //     //.append()
+    //     } else {
+    //     console.log("Not a SwayzeDay");
+    //     //.append()
+    //   }
+    // }
+    // console.log(whatFilm,data);
+
+
+});
