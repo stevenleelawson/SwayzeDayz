0f05316d4ae415667f1896b1e521a9f0e259549b
Steven Lawson <unluckygamblermusic@gmail.com>
2016-05-18 11:28:33 -0600

    enter now submits, with overview of movie

--- data.js
+++ data.js
@@ -9,6 +9,16 @@ $(document).ready(function (){
     });
 
   });
+  $(".painInTheAss").keyup(function(event) {
+    if (event.keyCode == 13) {
+      var inputMovie = $('#1').val().toLowerCase();
+      console.log("TEST");
+      // console.log(inputMovie);
+      getData().then(function(movieResults){
+        compareMovie(movieResults,inputMovie);
+      })
+    }
+  });
   function getData (){
        var p1 = $.get("https://api.themoviedb.org/3/discover/movie?with_cast=723&api_key=426006a23834affecd834cfdf4937e2c")
     //  .done(function(data) {
@@ -29,28 +39,49 @@ $(document).ready(function (){
 
   function compareMovie (movieResults,inputMovie){
     var SwayzeDay=false;
+    console.log(movieResults)
     // var SwayzeDay=(title===inputMovie);
     for(i=0; i<movieResults.length; i++){
       // console.log(data[i].results);
       for (var j = 0; j < movieResults[i].results.length; j++) {
         // console.log(data[i].results[j]);
-        var title=movieResults[i].results[j].title.toLowerCase();
+        // var title=movieResults[i].results[j].title.toLowerCase();
+        // var overView=movieResults[i].results[j].overview;
         // console.log(data[i].results[j].title);
-        if(title===inputMovie){
+        if(inputMovie===movieResults[i].results[j].title.toLowerCase()){
+// WHAT DO YOU WANY ME TO DO //
           SwayzeDay=true;
+          $('.overView').empty();
+          $('.overView').append(movieResults[i].results[j].overview);
+
         }
+
+
       }
     }
-    $('.ahpend').empty()
-  if(SwayzeDay===false){
-
-    $('.ahpend').append("Not A SwayzeDay!")
-  } else {
-    
-
-    $('.ahpend').append("SwayzeDay")
-  }
-}
+      $('.ahpend').empty();
+      if (SwayzeDay===false){
+        $('.ahpend').append("Not a SwayzeDay!")
+        $('body').css('background-color', 'pink')
+      } else {
+        // $('.overView').append(movieResults[i].results[j].overview)
+        $('.ahpend').append("SwayzeDay!!!!")
+        $('body').css('background-color', 'blue')
+      }
+    }
+  })
+//     }
+//     $('.ahpend').empty()
+//   if(SwayzeDay===false){
+//
+//     $('.ahpend').append("Not A SwayzeDay!")
+//   } else {
+//
+//
+//     $('.ahpend').append("SwayzeDay")
+//     $('.ahpend').append(overView);
+//   }
+// }
     // console.log(whatFilm);
     // results=dataresults;
     // var title;
@@ -68,5 +99,5 @@ $(document).ready(function (){
     // }
     // console.log(whatFilm,data);
 
-
-});
+//
+// });
